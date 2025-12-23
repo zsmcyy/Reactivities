7b7f8367af674a79cc4919d26c56759938675af9
zsmcyy <48477234@qq.com>
2025-12-23 15:20:48 +0800

    第02章结束

---
+++ Domain/Activity.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain;
+
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { get; set; }
+    public DateTime Date { get; set; }
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public bool IsCancelled { get; set; }
+    
+    // location props 位置:城市\场地\经纬度
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public required DbSet<Activity> Activities { get; set; }
+}
